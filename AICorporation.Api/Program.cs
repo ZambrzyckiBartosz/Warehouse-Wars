b5f531d9937ab5f24e89214c59c8f1a6b103439c
ZambrzyckiBartosz <zambrzyckibartosz709@gmail.com>
2026-04-03 19:58:28 +0200

    DbBaseImplementation

--- AICorporation.Api/Program.cs
+++ AICorporation.Api/Program.cs
@@ -1,10 +1,14 @@
 using AICorporation.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using AICorporation.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 var game = new Company("firma 1", 5000000m, new List<Building>());
 var testWreHouse = new Warehouse(1,"testWreHouse", 1, 10000,1,10,0);
 game.BuyBuilding(testWreHouse, 0);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")).UseSnakeCaseNamingConvention());
 builder.Services.AddControllers();
 builder.Services.AddSingleton(game);
 builder.Services.AddHostedService<GameEngineService>();
---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AICorporation.Infrastructure;
+public class User
+{
+    public int id { get; set; }
+    public string? username { get; set; }
+    public string? password { get; set; }
+    public List<Inventory>? inventory { get; set; }
+}
+
+public class Inventory
+{
+    public int id { get; set; }
+    public int userid { get; set; }
+    public int warehouseid { get; set; }
+    public Warehouse? warehouse { get; set; }
+    public int quantity { get; set; }
+}
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) :  base(options) {}
+    public DbSet<User> Users { get; set; }
+    public DbSet<Warehouse> Warehouses { get; set; }
+    public DbSet<Inventory> Users_Inventory { get; set; }
+}
